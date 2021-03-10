0c11300fe9e8516decc91162b7213dc0aca108be
Виктория <viktoriya.pimkova@mail.ru>
2021-03-10 19:00:46 +0300

    начало работы с React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app/';
+//
+// class WhoAmI extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             years: 26
+//         }
+//         this.nextYear = () => {
+//             this.setState(state => ({
+//                 years: ++state.years
+//             }))
+//         }
+//     }
+//
+//     render() {
+//         const {name, surname, link} = this.props;
+//         const {years} = this.state;
+//         return (
+//             <>
+//                 <button onClick={this.nextYear}>++</button>
+//                 <h1>My name is {name}, surname - {surname}, years = {years}</h1>
+//                 <a href={link}>My profile</a>
+//             </>
+//         )
+//     }
+// }
+//
+// const All = () => {
+//     return (
+//         <>
+//             <WhoAmI name="Vikulya" surname="Zhivagina" link="https://www.instagram.com/viktoriyapm/"/>
+//             <WhoAmI name="Egor" surname="Zhivagin" link="https://www.instagram.com/viktoriyapm/"/>
+//             <WhoAmI name="John" surname="Zhivagin" link="https://www.instagram.com/viktoriyapm/"/>
+//         </>
+//     )
+// }
 
 ReactDOM.render(
+    // <All/>,
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
